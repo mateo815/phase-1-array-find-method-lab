2c00812c820158d84afc8cf0576e621843ea90eb
Matt Quinn <mjq0533@gmail.com>
2022-01-17 08:55:57 -0600

    commit tree 2a43f8da0368a5fa128ba9867459231ba1cb5d03

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // code your solution here
-superbowlWin = (record) => {
-    const result = record.find( record => record.result === "W" );
-    return !!result ? result.year : undefined;
-  }
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+function superbowlWin(record) {
+  const result = record.find(record => record.result === "W");
+  return !!result ? result.year : undefined;
+
+}
+
+superbowlWin(record)
